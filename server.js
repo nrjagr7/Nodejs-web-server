221db84c03f96950bd3418fea417b77cd5ddc1a7
Neeraj Agrawal <nrjagr7@gmail.com>
2019-01-23 15:14:17 +0530

    setup start srcipt and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
+const port =process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -58,7 +60,7 @@ app.get('/about', (req,res) => {
    //res.send('this is about section');
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
 
-  console.log('Server up on port 3000.');
+  console.log(`Server up on port ${port}`);
 });
